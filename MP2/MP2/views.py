158cde7e9a58fff458c2f7839eaa82e94372b850
JBringas <jarold_bringas@dlsu.edu.ph>
2017-07-22 19:48:16 +0800

    homepage posts w/ correct user url

--- MP2/MP2/views.py
+++ MP2/MP2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from userprof.models import Post
+from userprof.models import User
 
 def index(request):
     #ada = '<h1>This is the Home page</h1>'
@@ -8,7 +10,10 @@ def index(request):
     #ada += '<a href="/users/">User Profiles</a>'
     #return HttpResponse(ada)
     #template = loader.get_template('userprof/templates/homepage/homepage.html')
+    all_post = Post.objects.all()
+    all_user = User.objects.all()
     context = {
-        
+        'all_p' : all_post,
+        'all_user' : all_user,
     }
     return render(request, 'homepage/Mainhpage.html', context)

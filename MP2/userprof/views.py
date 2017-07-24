93a90815e0ffc6f80efa4a253c59c0f57bb817bc
JBringas <jarold_bringas@dlsu.edu.ph>
2017-07-24 20:24:15 +0800

    arranged userprof templates

--- MP2/userprof/views.py
+++ MP2/userprof/views.py
@@ -5,9 +5,9 @@ from .models import Post
 def index(request):
     all_users = User.objects.all()
     return render(request, 'homepage/userhpage.html', {'all_u' : all_users,
-        '/' : '/',})
+        '/' : '/', 'currdirect' : 2})
 
 def user(request, user_num):
     sUser = get_object_or_404(User,id=user_num)
     return render(request, 'usertemp/user.html', {'user' : sUser,
-        'all_upost' : sUser.post_set.all()})
+        'all_upost' : sUser.post_set.all(), 'currdirect' : 3})

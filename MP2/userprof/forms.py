82e9c1add3253334697b19316bc49ca6a04cf1ee
Densolator <raffaeld12@gmail.com>
2017-07-27 16:24:04 +0800

    .

--- MP2/userprof/forms.py
+++ MP2/userprof/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length = 30, required = True, help_text = 'Required.')
@@ -12,3 +13,6 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','first_name', 'last_name', 'degree' ,'email', 'password1', 'password2']
+        help_texts = {
+            'password1': _('Your password cannot be too similar to your other personal information. Your password must contain at least 8 characters. Your password cannot be a commonly used password. Your password cannot be entirely numeric.'),
+        }
